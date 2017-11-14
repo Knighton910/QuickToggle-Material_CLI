455e4903bac369ffc7eca1f25b270e6e0b54f104
kelvin  knighton <keltheceo@gmail.com>
2017-11-13 23:14:20 -0800

    1st awesome commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
